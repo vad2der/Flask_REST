6dce91a7ff9b9c2b1b82130d6fa46cf3764d8aad
vad2der <vad.deryabin@gmail.com>
2017-04-05 10:41:12 -0600

    set up the app as a layer before another API

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, Request, render_template
 from flask_restful import Api, Resource, fields, marshal_with
+import requests
+import simplejson
 
 app = Flask(__name__)
 api = Api(app)
 
-output_fields = {"field1": fields.Integer,
-                "field2": fields.Float,
-                "field3": fields.Float}
+output_fields = {"id": fields.Integer,
+                "description": fields.String,
+                "price": fields.Float,
+                "published_date": fields.String,
+                "seller_latitude": fields.Float,
+                "seller_longitude": fields.Float,
+                "seller_name": fields.String,
+                "status": fields.String,
+                "title": fields.String}
 
 
 class Item_api(Resource):
@@ -14,8 +22,11 @@ class Item_api(Resource):
     class for API
     """
     @marshal_with(output_fields)
-    def get(self):
-        pass
+    def get(self, param):
+        if param == "all":
+            output = requests.get("https://item-api.herokuapp.com/api/v1/items").text
+            output = simplejson.load(ouptut)
+            return output, 200
 
     def post(self):
         pass
@@ -26,7 +37,7 @@ class Item_api(Resource):
     def update(self):
         pass
 
-api.add_resource(Item_api, '/api/v1/item/')
+api.add_resource(Item_api, '/api/v1/item/<param>')
 
 if __name__ == "__main__":    
     app.run(port=5000, debug=True)
